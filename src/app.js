40673edd9c83490744361c56c98b89ef9f754bab
only-zover <j0seextremehd@outlook.com>
2023-01-17 12:19:21 -0300

    add css

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.use("/", indexRouter);
 app.use("/posts", postRouter);
