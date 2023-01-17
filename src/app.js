f8cc6d21d84958f1c74c28a58ab47a59873ed22b
only-zover <j0seextremehd@outlook.com>
2023-01-17 10:44:28 -0300

    connect mongodb

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 const indexRouter = require("./routes/index");
 const postRouter = require("./routes/posts");
 
+const DATABASE = process.env.DATABASE;
 const app = express();
+
+mongoose.set("strictQuery", false);
+mongoose.connect(DATABASE);
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/", indexRouter);
 app.use("/posts", postRouter);
