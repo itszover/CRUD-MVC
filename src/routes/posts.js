1da83097e282494281efeaf2fa231814b32d1e8b
only-zover <j0seextremehd@outlook.com>
2023-01-17 08:46:38 -0300

    pug layout config

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("posts");
+  res.render("posts", { title: "Posts" });
 });
 
 router.get("/edit", (req, res) => {
-  res.render("edit");
+  res.render("edit", { title: "Edit" });
 });
 
 module.exports = router;
