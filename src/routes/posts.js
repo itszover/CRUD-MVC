37b77a53467dd58e2b860db99a395e5d606b00a1
only-zover <j0seextremehd@outlook.com>
2023-01-18 17:37:32 -0300

    include,  models e ++

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,12 +1,35 @@
 const express = require("express");
 const router = express.Router();
 
+const Post = require("../models/post");
+
 router.get("/", (req, res) => {
-  res.render("posts", { title: "Posts" });
+  const posts = [
+    {
+      title: "Teste",
+      createdAt: new Date(),
+      description: "Teste desc",
+    },
+    {
+      title: "Teste2",
+      createdAt: new Date(),
+      description: "Teste desc2",
+    },
+  ];
+
+  res.render("posts/posts", { title: "Posts", posts: posts });
+});
+
+router.post("/", (req, res) => {
+  const post = new Post({});
 });
 
 router.get("/edit", (req, res) => {
-  res.render("edit", { title: "Edit" });
+  res.render("posts/edit", { title: "Edit" });
+});
+
+router.get("/new", (req, res) => {
+  res.render("posts/new", { title: "Nova postagem" });
 });
 
 module.exports = router;
