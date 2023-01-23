92115bc879da11ee1983f7f7576d43a4a1ce0bab
only-zover <j0seextremehd@outlook.com>
2023-01-23 00:14:20 -0300

    ???

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Post = require("../models/Post");
+const Post = require("./../models/Post");
 
 router.get("/", async (req, res) => {
   const posts = await Post.find().sort({ createdAt: "desc" });
